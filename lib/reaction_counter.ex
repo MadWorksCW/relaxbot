768607bd87a77fcc687a22ce5657448fc244f9dc
Craig Smith <inicsmith@amagisoft.com>
2016-10-23 15:52:07 -0500

    init reaction_counter

---
+++ lib/reaction_counter.ex
@@ -0,0 +1,69 @@
+defmodule ReactionCounter do
+  use GenServer
+
+  ## Client API
+
+  @doc """
+  Starts the registry.
+  """
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  @doc """
+  Looks up the pid for `message_id` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, message_id) do
+    GenServer.call(server, {:lookup, message_id})
+  end
+
+  @doc """
+  Adds a message_id if it doesn't exist, increments the reaction count if it does.
+  """
+  def add(server, message_id) do
+    GenServer.cast(server, {:add, message_id})
+  end
+
+  @doc """
+  decrements the message_id count if the message_id exists and has a count > 0.
+  """
+  def remove(server, message_id) do
+    GenServer.cast(server, {:remove, message_id})
+  end
+
+  ## Server Callbacks
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:lookup, message_id}, _from, message_ids) do
+    {:reply, Map.fetch(message_ids, message_id), message_ids}
+  end
+
+  def handle_cast({:add, message_id}, message_ids) do
+    if Map.has_key?(message_ids, message_id) do
+    	message_ids = Map.put(message_ids, message_id, Map.get(message_ids, message_id) + 1)
+      {:noreply, message_ids}
+    else
+      {:noreply, Map.put(message_ids, message_id, 1)}
+    end
+  end
+
+  def handle_cast({:remove, message_id}, message_ids) do
+    if Map.has_key?(message_ids, message_id) do
+    	tweet_count = Map.get(message_ids, message_id)
+    	if ( tweet_count > 0 ) do 
+    		{:noreply, Map.put(message_ids, message_id, tweet_count - 1)}
+    	else 
+    		{:noreply, message_ids}	
+    	end
+    else
+    	# pass through, shouldn't remove if if doesn't have a key
+      {:noreply, message_ids}
+    end
+  end
+
+end
+
